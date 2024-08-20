5227b18da0fee37b657a4077ff87d2747c093e89
Yunus Emre Can Sümeralp <39301904+emresmrlp@users.noreply.github.com>
2024-08-20 12:57:04 +0300

    Add files via upload

---
+++ 1768_MergeStringsAlternately/success_1.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+
+char * mergeAlternately(char * word1, char * word2)
+{
+	if (word1 == NULL || word2 == NULL)
+		return NULL;
+	int word1_size = strlen(word1);
+	int word2_size =  strlen(word2);
+	int size = word1_size + word2_size + 1;
+	char *buffer = (char *)malloc(sizeof(char) * size);
+	if (buffer == NULL)
+			return NULL;
+	int buffer_index = 0;
+	int i = 0;
+	int j = 0;
+	while (i < word1_size || j < word2_size)
+	{
+		if (i < word1_size)
+			buffer[buffer_index++] = word1[i++];
+		if (j < word2_size)
+			buffer[buffer_index++] = word2[j++];
+	}
+	buffer[buffer_index] = '\0';
+	return buffer;
+}
---
+++ 1_TwoSum/success_1.c
@@ -0,0 +1,32 @@
+// Success
+
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	*returnSize = 2;
+	int *result = (int *)malloc(2 * sizeof(int));
+	if (result == NULL)
+	{
+		*returnSize = 0;
+		return NULL;
+	}
+	int i = 0;
+	while (i < numsSize - 1)
+	{
+		int j = i + 1;
+		while (j < numsSize)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				result[0] = i;
+				result[1] = j;
+				return result;
+			}
+			j++;
+		}
+		i++;
+	}
+	*returnSize = 0;
+	free(result);
+	return NULL;
+}
